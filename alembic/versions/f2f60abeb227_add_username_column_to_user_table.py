20012d41c7ee115ded3f7c2d59a2f4fe3e61d01c
Tomiwa-dev <adewoletomiwa@gmail.com>
2022-03-14 14:38:39 +0100

    added username column to Users table

---
+++ alembic/versions/f2f60abeb227_add_username_column_to_user_table.py
@@ -0,0 +1,27 @@
+"""add username column to user table
+
+Revision ID: f2f60abeb227
+Revises: a2a8e1270a3e
+Create Date: 2022-03-14 14:32:26.645243
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'f2f60abeb227'
+down_revision = 'a2a8e1270a3e'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    op.add_column('Users', sa.Column(
+        'username', sa.Boolean(), nullable=False), )
+    pass
+
+
+def downgrade():
+    op.drop_column('Users', 'username')
+    pass
